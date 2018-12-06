2703c2a9ea6fdd9a68d0f51461ff7a86f7275295
Michail Knyrevic <michail@webilize.com>
2018-12-06 10:56:49 +0100

    Add project files.

---
+++ Advent2018-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent2018_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
